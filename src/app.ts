fd7caf4f4e5e143379fb45e78d48e0e5cb9fad1f
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-20 12:37:23 +0600

    route setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
-import express from "express";
-
+import express from 'express';
+import cookieParser from 'cookie-parser';
 const app = express();
+import cors from 'cors';
+import router from './app/routes';
 
+//perser
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
-app.get("/", (req, res) => {
-  res.send("Hello, Welcome to the STN blog Server!");
+//application routes
+app.use('/api', router);
+
+//checking
+app.get('/', (req, res) => {
+  res.send('Hello, Welcome to the STN blog Server!');
 });
 
 export default app;
