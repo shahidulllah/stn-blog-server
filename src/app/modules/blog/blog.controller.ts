f1fd067768639b6f65b7269eddb9e6a98d3976d1
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-21 17:54:49 +0600

    update blog controller

--- src/app/modules/blog/blog.controller.ts
+++ src/app/modules/blog/blog.controller.ts
@@ -1,33 +1,64 @@
-import { StatusCodes } from "http-status-codes";
-import AppError from "../../errors/appError";
-import catchAsync from "../../utils/catchAsync";
-import { createBlogValidationSchema } from "./blog.validation";
-import { createBlog } from "./blog.service";
-import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/appError';
+import catchAsync from '../../utils/catchAsync';
+import {
+  createBlogValidationSchema,
+  updateBlogValidationSchema,
+} from './blog.validation';
+import { createBlog, updateBlog } from './blog.service';
+import sendResponse from '../../utils/sendResponse';
 
-export const createBlogController = catchAsync(async(req, res) => {
-    const userId = req.user?.id;
+export const createBlogController = catchAsync(async (req, res) => {
+  const userId = req.user?.id;
 
-    if(!userId){
-        throw new AppError(StatusCodes.BAD_REQUEST, "Unauthorized access")
-    }
+  if (!userId) {
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Unauthorized access');
+  }
 
-    const {title, content} = req.body;
+  const { title, content } = req.body;
 
-    //validate request data
-    const validation = createBlogValidationSchema.safeParse({title, content})
+  //validate request data
+  const validation = createBlogValidationSchema.safeParse({ title, content });
 
-    if(!validation){
-        throw new AppError(StatusCodes.BAD_REQUEST, "Validation Error")
-    }
+  if (!validation) {
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Validation Error');
+  }
 
-    //create blog
-    const newBlog = await createBlog(title, content, userId)
+  //create blog
+  const newBlog = await createBlog(title, content, userId);
 
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: "Blog created successfully",
-        data: newBlog,
-    })
-})
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Blog created successfully',
+    data: newBlog,
+  });
+});
+
+export const updateBlogController = catchAsync(async (req, res) => {
+  const userId = req.user?.id;
+
+  if (!userId) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'Unauthorized access');
+  }
+
+  const { id: blogId } = req.params;
+  const { title, content } = req.body;
+
+  //validate request data
+  const validation = updateBlogValidationSchema.safeParse({ title, content });
+
+  if (!validation.success) {
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Validation error');
+  }
+
+  //update the blog
+  const updatedBlog = await updateBlog(blogId, userId, validation.data);
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Blog updated successfully',
+    data: updatedBlog,
+  });
+});
--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -1,3 +1,5 @@
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/appError';
 import { TBlog } from './blog.interface';
 import { BlogModel } from './blog.model';
 
@@ -12,3 +14,17 @@ export const createBlog = async (
 
   return newBlog;
 };
+
+
+export const updateBlog = async (blogId: string, userId: string, updateData: Partial<TBlog>): Promise<TBlog | null> => {
+    const blog = await BlogModel.findOne({_id: blogId, author: userId});
+
+    if(!blog) {
+        throw new AppError(StatusCodes.BAD_REQUEST,"Blog not found or you are not the author")
+    }
+
+    Object.assign(blog, updateData);
+    await blog.save();
+
+    return blog;
+}
