e9a19180f8f5a91d2187538cdb6339786c7a30fd
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-21 09:38:18 +0600

    auth utils

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,18 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = Router();
 
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: ,
+  },
+  {
+    path: '/blogs',
+    route: ,
+  },
+];
 
+moduleRoutes.forEach(route => router.use(route.path, route.route));
 
 export default router;
