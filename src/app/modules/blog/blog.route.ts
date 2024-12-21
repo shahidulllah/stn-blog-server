97452970fa577581439384125db8326cf7039ad8
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-21 19:16:53 +0600

    code modify

--- src/app/modules/blog/blog.route.ts
+++ src/app/modules/blog/blog.route.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-import { createBlogController, deleteBlogController, getAllBlogsController, updateBlogController } from './blog.controller';
+import {
+  createBlogController,
+  deleteBlogController,
+  getAllBlogsController,
+  updateBlogController,
+} from './blog.controller';
 import { authMiddleware } from '../../middlewares/auth';
-
 
 const router = express.Router();
 
