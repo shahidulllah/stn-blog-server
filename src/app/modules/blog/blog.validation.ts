8b03a073abd6adccf82c1c7fd983665e81c1417e
shahidulllah <mdshahidsumon177@gmail.com>
2024-12-21 17:35:37 +0600

    blog update validation

--- src/app/modules/blog/blog.validation.ts
+++ src/app/modules/blog/blog.validation.ts
@@ -4,3 +4,8 @@ export const createBlogValidationSchema = z.object({
   title: z.string().min(1, 'Title is required'),
   content: z.string().min(1, 'Content is required'),
 });
+
+export const updateBlogValidationSchema = z.object({
+    title: z.string().optional(),
+    content: z.string().optional(),
+  });
